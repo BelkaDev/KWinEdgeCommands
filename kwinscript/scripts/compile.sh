6f989b1a00fba6a29ec6ebfd05b0ce57d1e0b30c
Belka <belkacem.hadjoudj@esprit.tn>
2022-03-13 14:45:30 +0100

    dump script

--- kwinscript/scripts/compile.sh
+++ kwinscript/scripts/compile.sh
@@ -1,4 +1,4 @@
- in=/home/belka/Dbus_script/kwinscript/contents/code/source.template.js
- out=/home/belka/Dbus_script/kwinscript/contents/code/source.js
+ in=contents/code/source.template.js
+ out=contents/code/source.js
  action="$(grep -oP '(?<=X-KDE-PluginInfo-Name=).*' metadata.desktop)"
  cat $in | sed -e "s/\${COMMANDNAME}/\"${action}\"/g" > $out
